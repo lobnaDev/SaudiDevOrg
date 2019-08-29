6bcdfa7aa71e8d158fa99c86c9f2baf1575e7210
lobnaDev <lobna.m.b@gmail.com>
2019-08-29 12:44:51 +0300

    Add files via upload
    week one task

---
+++ weekone.py
@@ -0,0 +1,21 @@
+import random
+print("hello world")
+x = "awesome"  """ Variable """
+print("paython is "+ x)
+x = 10
+y = 5
+print(x+y)
+
+a = 12
+b = 1.80
+c= 50j
+#Type conversion
+x = float(a)
+z = int(b)
+i =complex(a)
+print (x)
+print (z)
+print (i)
+print(type(a))
+print(type(b))
+print (random.randrange(1,10))
